9ec7782352004b686ec53c2d223231d383e75e9f
DhinakaranS-75 <dhinakaran75493@gmail.com>
2023-11-19 00:25:10 +0530

    CAREER FAIR - PROJECT-BACKEND

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const connectDb = require("./db");
+const signinRouter = require("./Routes/signin");
+const loginRouter = require("./Routes/login");
+const homeRouter = require("./Routes/home");
+
+const PORT = 4000;
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+connectDb();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.use("/signin", signinRouter);
+app.use("/login", loginRouter);
+app.use("/home", homeRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
